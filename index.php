340a6bc7e8572067e6b35d30a5545c94cf65c064
Rick Hurst <rick@olivewoodtech.co.uk>
2013-03-12 07:15:28 +0000

    moved app code to app.js - nav items are now bootstrapped

--- index.php
+++ index.php
@@ -61,69 +61,18 @@
 		<script src="/js/lib/underscore-min.js"></script>
 		<script src="/js/lib/backbone-min.js"></script>
 		<script src="/js/lib/backbone.marionette.min.js"></script>
+
+		<script src="/js/app/app.js"></script>
 		<script>
-		MyApp = new Backbone.Marionette.Application();
-
-	    MyApp.addRegions({
-	        headerRegion: "header",
-	        mainRegion: "section.main",
-	        sidebarRegion: "section.aside",
-	        footerRegion: "footer"
-	    });
-
-	    MyApp.defaultContentView = Backbone.Marionette.ItemView.extend({
-	    	template:'#default-content'
-	    });
-
-	    MyApp.defaultFooterContentView = Backbone.Marionette.ItemView.extend({
-	    	template:'#default-footer-content'
-	    });
-
-	   	MyApp.defaultHeaderContentView = Backbone.Marionette.ItemView.extend({
-	    	template:'#default-header-content'
-	    });
-
-	   	// side nav
-	   	MyApp.NavLink = Backbone.Model.extend({});
-	   	MyApp.NavLinks = Backbone.Collection.extend({
-	   		model: MyApp.NavLink
-	   	});
-
-	    MyApp.SideNavItemView = Backbone.Marionette.ItemView.extend({
-	    	template: '#sidebar-nav-item',
-	    	tagName: 'li'
-	    });
-
-	    MyApp.SideNavView = Backbone.Marionette.CompositeView.extend({
-	    	template:'#sidebar-nav',
-	    	itemView: MyApp.SideNavItemView,
-	    	itemViewContainer: 'ul',
-	    	tagName: 'nav'
-	    });
 
 	    var navLinks = new MyApp.NavLinks([
 	    	new MyApp.NavLink({text: 'foo'}),
 	    	new MyApp.NavLink({text: 'bar'})
 	    ]);
 
-	    var defaultContentView = new MyApp.defaultContentView();
-	    var defaultFooterContentView = new MyApp.defaultFooterContentView();
-	    var defaultHeaderContentView = new MyApp.defaultHeaderContentView();
-
-	    var sideNavView = new MyApp.SideNavView({
-	    	collection: navLinks
-	    });
-
-	    MyApp.mainRegion.show(defaultContentView);
-	    MyApp.footerRegion.show(defaultFooterContentView);
-	    MyApp.sidebarRegion.show(sideNavView);
-	    MyApp.headerRegion.show(defaultHeaderContentView);
-
 	    $(document).ready(function(){
-			MyApp.start();
+			MyApp.start({navItems:navLinks});
 		});
-
-
 
 		</script>
 	</body>
